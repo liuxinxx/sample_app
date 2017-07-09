4863f9d8a22bc8eeaf8e39ec158370c3adcac0e3
myvary <727484328@qq.com>
2017-07-09 16:55:03 +0800

    add test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  #控制器中的动作路由
   get 'static_pages/home'
 
   get 'static_pages/help'
-
+  get 'static_pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
