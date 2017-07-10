430e225eb7298cbcf98b8fec15d44d79db2cb6eb
myvary <727484328@qq.com>
2017-07-10 09:43:00 +0800

    add contact_page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
   get 'static_pages/about'
+  get 'static_pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'static_pages#home'#根路由的指向
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  # test "should get root" do
+  #   get FILL_IN
+  #   assert_response FILL_IN
+  # end
+  #测试跟路由出错。。。
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -16,5 +22,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 end
