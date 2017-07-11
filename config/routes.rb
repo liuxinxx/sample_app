e4383c972d918faccf75eea7683397f4e39073cd
myvary <727484328@qq.com>
2017-07-11 11:18:43 +0800

    添加注册验证

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get '/signup',  to:'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'#根路由的指向
+  resources :users
 end
