4729eb36b664beafb085485e6c0fa946e89798b4
myvary <727484328@qq.com>
2017-08-16 20:20:12 +0800

    将lib/proxy添加到配置文件

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.eager_load_paths += [
+      Rails.root.join('lib/get_proxy')
+    ]
   end
 end
